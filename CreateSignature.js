2befa85ddf01c8aac830c417136a350dd43fae9b
Steffan Boodhoo <sboodho@corp.tstt.local>
2019-03-01 12:07:40 -0400

    wrapper built, pre-production build

--- CreateSignature.js
+++ CreateSignature.js
@@ -3,87 +3,31 @@
 // import jsPDF from 'jspdf';
 // import SignaturePad from 'signature_pad';
 
-let LINES = 0, VSPACE = 5;//5mm
-let LINE_max = 68;
-let DOC = null;
+// let LINES = 0, VSPACE = 5;//5mm
+// let LINE_max = 68;
+// let DOC = null;
 window.onload = () => {
-    let signaturePad = init('signature_canvas');
-
+    // let signaturePad = init('signature_canvas');
+    let doc = new Document('signature_canvas')
     //Save Signature Button
     document.getElementById('btn_signature_add').onclick = () => {
-        // add_signature(doc, signaturePad);
-        add_signature(signaturePad);
+        // add_signature({ signaturePad });
+        let options = { fit: [100, 100], align: 'center' };
+        doc.add_signature(options);
+        doc.skip_lines(3)
+        // add_signature({ signaturePad, options });
+        // options = { cover: [100, 100], align: 'center' };
+        // add_signature({ signaturePad, options });
+
     };
     //Save Signature Button
     document.getElementById('btn_pdf_generate').onclick = () => {
-        // save_signature(signaturePad)
-        // generate_document(doc);
-        generate_document();
+        doc.finish_document();
     };
+    
     //Add Text Button
     document.getElementById('btn_add_text').onclick = () => {
         const text = document.getElementById('input_text').value;
-        add_text(text);
+        doc.add_text(text);
     }
-}
-
-const init = (canvas_id) => {
-    //CANVAS SIGNATURE SETUP
-    const canvas = document.getElementById(canvas_id);
-    let signaturePad = new SignaturePad(canvas);
-
-    DOC = new PDFDocument;
-    //Create new stream for document
-    create_stream(DOC);
-    return signaturePad;
-}
-const create_stream = (doc) => {
-    let stream = doc.pipe(blobStream());
-    stream.on('finish', () => {
-        let a = document.createElement("a");
-        a.style = "display: none";
-        document.body.appendChild(a);
-
-        const blob = stream.toBlob('application/pdf');
-        const url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = 'test'
-        a.click();
-        window.URL.revokeObjectURL(url);
-    })
-    // return stream;
-    // console.log('%cSuccessfully initialised signature and pdf objects', 'color:green');
-}
-
-const generate_document = () => {
-    DOC.end()
-    //Create new stream for document
-    DOC = new PDFDocument();
-    STREAM = create_stream(DOC);
-}
-
-const add_signature = ( signaturePad, image = null, format = null) => {
-    if (image == null) {
-        image = signaturePad.toDataURL();
-        format = 'PNG';
-    }
-    DOC.image(image, 0, (1 + LINES) * VSPACE);
-    LINES += 1 + 150 / (3.7795275591 * VSPACE)  //TODO 150 is number of pixels in canvas will have to change
-
-}
-
-const add_text = (text) => {
-    DOC.text(text);
-    DOC.moveDown();
-    // let lines = [];
-    // for (let i = 0; i <= text.length; i = i + LINE_max)
-    //     lines.push(text.slice(i - LINE_max, i))
-    // if (lines.length == 0)
-    //     lines.push(text)
-
-    // for (line in lines) {
-    //     console.log(line)
-    //     DOC.text(line, 0, (1 + LINES) * VSPACE);
-    //     LINES++;
-    // }
 }
---
+++ CustomDocument.js
@@ -0,0 +1,70 @@
+import SignaturePad from 'signature_pad';
+import PDFDocument from 'pdfkit';
+class Document {
+    constructor(canvas_id, options) {
+        this.signaturePad = null;
+        this.DOC = null;
+        this.custom_handle_end_stream = null;
+        this.document_name = 'untitled';
+        //CANVAS SIGNATURE SETUP
+        const canvas = document.getElementById(canvas_id);
+        this.signaturePad = new SignaturePad(canvas);
+
+        this.DOC = new PDFDocument;
+        //Create new stream for document
+        this.create_stream(this.DOC);
+    }
+
+    add_text(text) {
+        this.DOC.text(text);
+        this.DOC.moveDown()
+    }
+
+    skip_lines(lines) { this.DOC.moveDown(lines); }
+
+    add_image(image, options) { this.attach_image_to_document(image, options); }
+
+    add_signature(options) { this.attach_image_to_document(this.signaturePad.toDataURL(), options); }
+
+    attach_image_to_document(image, options) {
+        if (options != undefined)
+            this.DOC.image(image, options);
+        else
+            this.DOC.image(image);
+    }
+
+    finish_document() {
+        this.DOC.end();
+        this.DOC = new PDFDocument();
+        this.create_stream(this.DOC);
+    }
+
+    set_handle_end_stream(func) { this.handle_end_stream = func; }
+
+    create_stream(doc) {
+        let stream = doc.pipe(blobStream());
+        stream.on('finish', () => {
+            const blob = stream.toBlob('application/pdf');
+            this.download_document(blob);
+        })
+    }
+
+    handle_end_stream (blob) {
+        if (this.custom_handle_end_stream == null)
+            download_document(blob)
+        else
+            this.custom_handle_end_stream(blob)
+    }
+
+    download_document (blob){
+        let a = document.createElement("a");
+        a.style = "display: none";
+        document.body.appendChild(a);
+        const url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = this.document_name;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+}
+export default Document;
